deea11e251860991eedbacea3aeb1ac877a45ffc
Davide Fasolo <davide.fasolo2@studio.unibo.it>
2024-02-29 17:00:19 +0100

    aggiunta connessione tra Finestra_generazione e Finestra_home

--- Finestra_generazione.py
+++ Finestra_generazione.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from Tkinter_methods import Valida_input
+from Tkinter_methods import Valida_input, Chiudi_finestra
 from Generazione import Genera
 from Scrivi import Scrivi_verifica_s
-
+from Finestra_home import Finestra_home
 
 
 def Finestra_generazione():
@@ -134,9 +134,15 @@ def Finestra_generazione():
         )
         Scrivi_verifica_s(verifica=verifica, classificazione = c)
 
+    def Bottone_home():
+        Chiudi_finestra(generazione)
+        Finestra_home()
 
-    button = tk.Button(generazione, text="Genera la verifica", command=Bottone_genera)
+    button = tk.Button(generazione,bg="orange" text="Genera la verifica", command=Bottone_genera)
     button.grid(row=12, column=4)
+
+    home_torna = tk.Button(generazione, bg ="yellow", text="Torna alla schermata home", command=Bottone_home)
+    home_torna.grid(row=12, column=0)
 
    # message.bind("<Button-1>", drag_start)
    # message.bind("<B1-Motion>", drag_motion)
