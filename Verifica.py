490cfc83c667dc9e1160266cb2cceb14f7433f50
Davide Fasolo <davide.fasolo2@studio.unibo.it>
2024-03-06 22:23:45 +0100

    documentazione classe Verifica

--- Verifica.py
+++ Verifica.py
@@ -11,9 +11,14 @@ class Verifica:
 
     def __init__(
             self,
-            n_esercizi : int = 1,
-            
+            n_esercizi : int = 1,            
     ):
+        """
+        Inizializza una nuova Verifica con il numero specificato di esercizi.
+
+        Args:
+            n_esercizi (int, optional): Il numero totale di esercizi nella verifica. Default è 1.
+        """
         self.n_esercizi = n_esercizi
         es = Esercizio()
         self.esercizi : List[Esercizio] = [es.copy() for _ in range(n_esercizi)]
@@ -23,8 +28,12 @@ class Verifica:
             ex: Esercizio
             ):
         """
-            Trova il primo elemento non inizializzato e sostituiscilo 
-            con l'esercizio dato in input
+        Aggiunge un nuovo esercizio alla verifica.
+
+        Trova il primo elemento non inizializzato e sostituiscilo con l'esercizio dato in input.
+
+        Args:
+            ex (Esercizio): L'esercizio da aggiungere alla verifica.
         """
         for i, es in enumerate(self.esercizi):
             if self.esercizio_std(es):
@@ -36,6 +45,15 @@ class Verifica:
         print(self.esercizi[0].testo)
 
     def esercizio_std(self, es: Esercizio):
+        """
+        Verifica se un esercizio è standard, ossia se tutti i suoi campi sono vuoti o impostati ai valori predefiniti.
+
+        Args:
+            es (Esercizio): L'esercizio da verificare.
+
+        Returns:
+            bool: True se l'esercizio è standard, False altrimenti.
+        """
         return (
             es.tematica == "" and
             es.testo == "" and
